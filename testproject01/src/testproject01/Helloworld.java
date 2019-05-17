e42d271cde0e6f9b8b74435b41f50b87b8be89a9
KanetaniK <boundforshinjyuku@yahoo.co.jp>
2019-05-17 21:48:10 +0900

    2019_05_17

---
+++ testproject01/src/testproject01/Helloworld.java
@@ -0,0 +1,14 @@
+package testproject01;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		do{
+			System.out.println(i);
+			i += 2;
+		}while(i <= 10);
+		
+	}
+}
