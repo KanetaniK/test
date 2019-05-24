996708a1cd9e72b858fdc7d58269f97ba0ce0012
sachiyuki <sachiyuki@outlook.jp>
2019-05-24 21:51:34 +0900

    2019_05_24

---
+++ Iphone/src/Android.java
@@ -0,0 +1,16 @@
+
+public class Android {
+
+	public static void main(String[] args){
+		SmartPhone android = new SmartPhone();
+		android.back();
+		android.call();
+		android.internet();
+		android.mail();
+		android.next();
+		android.photo();
+		android.play();
+		android.stop();
+	}
+
+}
