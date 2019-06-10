ba374d7a163d9fd3ee554186fd52268892af599f
tatsuhitomoriya <boundforshinjyuku@yahoo.co.jp>
2019-06-10 21:29:42 +0900

    2019_06_10

--- login/src/com/internousdev/login/util/DBConnector.java
+++ login/src/com/internousdev/login/util/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/logindb_yamada";
+	private static String url = "jdbc:mysql://localhost/logindb_kanetani";
 	private static String user = "root";
 	private static String password = "mysql";
 
