5040ecdd5b0ed7a681015d851d3c197c3decabe5
KanetaniK <boundforshinjyuku@yahoo.co.jp>
2019-07-05 21:43:17 +0900

    2019_07_05

--- WebProj4/src/com/internousdev/webproj4/dao/TestDAO.java
+++ WebProj4/src/com/internousdev/webproj4/dao/TestDAO.java
@@ -2,6 +2,7 @@ package com.internousdev.webproj4.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,7 @@ import com.internousdev.webproj4.util.DBConnector;
 
 public class TestDAO {
 
-	public List<LoginDTO> loginDTOList = new ArrayList<LoginDTO>();
+	public List<LoginDTO> LoginDTOList = new ArrayList<LoginDTO>();
 
 	public int insert(String username, String password){
 
@@ -45,6 +46,51 @@ public class TestDAO {
 
 		}
 		return ret;
+
+	}
+
+
+	public List<LoginDTO> select(String username, String password){
+
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "select * from users where user_name=? and password=?";
+		try{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()){
+				LoginDTO dto = new LoginDTO();
+				dto.setUsername(rs.getString("user_name"));
+				dto.setPassword(rs.getString("password"));
+				LoginDTOList.add(dto);
+			}
+
+			if(LoginDTOList.size() <= 0){
+				LoginDTO dto = new LoginDTO();
+				dto.setUsername("該当なし");
+				dto.setPassword("該当なし");
+				LoginDTOList.add(dto);
+			}
+		} catch(SQLException e){
+
+			e.printStackTrace();
+
+		}
+
+		try{
+
+			con.close();
+
+		} catch(SQLException e){
+
+			e.printStackTrace();
+
+		}
+		return LoginDTOList;
 
 	}
 
